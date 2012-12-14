cbe1fa25a8aafcddece44896fae16356d7e34ba8
Riey Hilliard <rileyhilliard@gmail.com>
2012-12-13 18:23:29 -0800

    Update db/migrate/20121214014510_add_camp.rb

--- db/migrate/20121214014510_add_camp.rb
+++ db/migrate/20121214014510_add_camp.rb
@@ -1,14 +1,4 @@
 class AddCamp < ActiveRecord::Migration
   def change
-    create_table :campaigns do |t|
-      t.text :name
-      t.text :address
-      t.decimal :longitude
-      t.decimal :latitude
-      t.boolean :gmaps
-      t.text :content
-        
-      t.timestamps
-    end
   end
 end
