f127d5cf84fe491b3de32fafc7680cfd494d5170
Riey Hilliard <rileyhilliard@gmail.com>
2012-12-13 18:20:44 -0800

    Update db/migrate/20121214011237_campaigns.rb

--- db/migrate/20121214011237_campaigns.rb
+++ db/migrate/20121214011237_campaigns.rb
@@ -1,14 +1,5 @@
 class Campaigns < ActiveRecord::Migration
   def change
-    create_table :campaigns do |t|
-      t.text :name
-      t.text :address
-      t.decimal :longitude
-      t.decimal :latitude
-      t.boolean :gmaps
-      t.text :content
-
-      t.timestamps
-    end
+    drop_table :campaigns 
   end
 end
