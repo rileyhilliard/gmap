6983489b7df1eccec3506d9acd6acff5b7bc488d
Riley Hilliard <rileyhilliard@gmail.com>
2012-12-13 18:25:13 -0800

    fix

--- db/migrate/20121212205717_create_campaigns.rb
+++ db/migrate/20121212205717_create_campaigns.rb
@@ -1,4 +1,14 @@
 class CreateCampaigns < ActiveRecord::Migration
   def change
+    create_table :campaigns do |t|
+      t.text :name
+      t.text :address
+      t.decimal :longitude
+      t.decimal :latitude
+      t.boolean :gmaps
+      t.text :content
+
+      t.timestamps
+    end
   end
 end
