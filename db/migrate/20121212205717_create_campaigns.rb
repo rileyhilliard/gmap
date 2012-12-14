92151692a06c9708ce1427d7645caffa7a9e1640
Riley Hilliard <rileyhilliard@gmail.com>
2012-12-13 17:56:40 -0800

    still trying to fix migration

--- db/migrate/20121212205717_create_campaigns.rb
+++ db/migrate/20121212205717_create_campaigns.rb
@@ -1,14 +1,5 @@
-class CreateCampaigns < ActiveRecord::Migration
+class DropCampaigns < ActiveRecord::Migration
   def change
-    create_table :campaigns do |t|
-      t.string :name
-      t.string :address
-      t.float :longitude
-      t.float :latitude
-      t.boolean :gmaps
-      t.string :content
-
-      t.timestamps
-    end
+    drop_table :campaigns 
   end
 end
